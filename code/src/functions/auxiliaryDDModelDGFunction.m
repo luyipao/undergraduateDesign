d9e3610f29b322b17eab8a30ee8cba27738ec8b5
luyipao <luyipao6@gmail.com>
2024-04-02 00:46:33 +0800

    debugging TVB limiter

--- code/src/functions/auxiliaryDDModelDGFunction.m
+++ code/src/functions/auxiliaryDDModelDGFunction.m
@@ -38,6 +38,37 @@ for j = 1:N
 end
 % cal C
 auxiliaryVarCoeffVec = A\F;
+
+%% init values
+func1Cells = cell(N,1);
+localAverageAuxq = zeros(N,1);
+for j = 1:N
+    func1 = @(x) 0*x;
+    for i = 1:n+1
+        [Pi,~] = legendreBaseFunction(i-1, mesh(j),mesh(j+1));
+        func1 = @(x) func1(x) + auxiliaryVarCoeffVec((j-1)*(n+1)+i) * Pi(x);
+    end
+    func1Cells{j} = func1;
+    localAverageAuxq(j) = quadgk(func1Cells{j}, mesh(j),mesh(j+1)) / (mesh(j+1)-mesh(j));
+end
+%%minmode limiter
+h = mesh(2) - mesh(1);
+M = 2 / 3 * 249*20*pi*20*pi * 20 * pi * 10e2 * h^2;
+auxqCoeffs = zeros(N*(n+1),1);
+for j = 1:N
+    func1 = func1Cells{j};
+    ur = func1(mesh(j+1)) - localAverageAuxq(j);
+    ul = localAverageAuxq(j) - func1(mesh(j));
+    urmod = localAverageAuxq(j) + minmod([ul localAverageAuxq(mod(j,N)+1)-localAverageAuxq(j) localAverageAuxq(j)-localAverageAuxq(mod(j-2+N,N)+1)], M);
+    ulmod =  localAverageAuxq(j) - minmod([ur localAverageAuxq(mod(j,N)+1)-localAverageAuxq(j) localAverageAuxq(j)-localAverageAuxq(mod(j-2+N,N)+1)], M);
+    auxqCoeffs((j-1)*(n+1) + 1) = (mesh(j+1)-mesh(j)) * localAverageAuxq(j) / sqrt(mesh(j+1)-mesh(j));
+    auxqCoeffs((j-1)*(n+1) + 2) = (urmod - ulmod) / (2*sqrt( 3/(mesh(j+1)-mesh(j)) ));
+    auxqCoeffs((j-1)*(n+1) + 3) = urmod - auxqCoeffs((j-1)*(n+1) + 1) / sqrt(mesh(j+1)-mesh(j)) - auxqCoeffs((j-1)*(n+1) + 2)*sqrt( 3/(mesh(j+1)-mesh(j)) );
+    auxqCoeffs((j-1)*(n+1) + 3)  = -auxqCoeffs((j-1)*(n+1) + 3) / sqrt( 5/(mesh(j+1)-mesh(j)) );
+    % electronConcentrationCoeffs((j-1)*(n+1) + 4) = 0;
+end
+auxiliaryVarCoeffVec = auxqCoeffs;
+
 %% output auxiliary function q
 auxq = @(x) 0 * x;
 
@@ -57,5 +88,11 @@ end
 end
 
 
-
+function y = minmod(v,M)
+if abs(v(1)) <= M
+    y = v(1);
+else
+    y = all(sign(v) == sign(v(1))) * sign(v(1)) * min(abs(v));
+end
+end
 
--- code/src/functions/diffDopingFunction.m
+++ code/src/functions/diffDopingFunction.m
@@ -1,5 +1,6 @@
 function y = diffDopingFunction(x)
 
 y = 20 * pi * 249 * (x>= 0.1 & x<= 0.15) .* cos(20*pi*(x-0.075)) ...
-    + 20 * pi * 249 * (x>= 0.45 & x<= 0.5) .* cos(20*pi*(x-0.075));
-y = y * 10e2
+    + 20 * pi * 249 * (x>= 0.45 & x<= 0.5) .* cos(20*pi*(x-0.475));
+y = y * 10e2;
+end
